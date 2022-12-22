35ef74d1f4ed8e45ce73377287de664977554e5c
icollinss <icthiongo@gmail.com>
2022-12-22 18:33:47 +0000

    Classes in JS

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+
+
+/* eslint-disable */
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
